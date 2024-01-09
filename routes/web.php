954d0d8afb0206fbc80f195fad9bfac2b571b9c4
MUSTFA MUSA <mmuu21itn@gmail.com>
2024-01-09 16:07:27 +0300

    1
    1

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,34 @@ use App\Models\Language;
 |
 */
 
+Route::get('/route-cache', function() {
+    \Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache
+Route::get('/config-cache', function() {
+    \Artisan::call('config:cache');
+    return 'Config cache cleared';
+});
+
+ // Clear application cache
+ Route::get('/clear-cache', function() {
+    \Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache
+Route::get('/view-clear', function() {
+    \Artisan::call('view:clear');
+    return 'View cache cleared';
+});
+
+// Clear cache using reoptimized class
+Route::get('/optimize-clear', function() {
+    \Artisan::call('optimize:clear');
+    return 'View cache cleared';
+});
 
     Route::group(['namespace' => 'Web','middleware'=>['XSS']],function(){
 
